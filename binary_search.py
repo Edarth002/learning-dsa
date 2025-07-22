dcb04fd10fc66391a30329ca7a9e902913155a59
Arthakker <arthuronyeanusi@gmail.com>
2025-07-22 09:18:29 +0100

    Resolved an issue in biinary_search

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,9 @@
+
+
+
 def binary_search(list, target):
     """
-    
+        Binary Search Al
     """
     first = 0
     last = len(list) - 1
@@ -8,15 +11,18 @@ def binary_search(list, target):
     while(first <= last):
         midpoint = (first + last) // 2
 
-        if(list[midpoint] == target):
-            return list[midpoint]
-        if(list[midpoint] > target):
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] > target:
             last = midpoint - 1
-        if(list[midpoint] < target):
+        else: 
             first = midpoint  + 1
     return "Target not found"
 
 list = [0,1,2,3,4,5,6]
+"""
+    target = 10
+"""
 target = 3
 
 print("The target is found at index: ",binary_search(list, target))
