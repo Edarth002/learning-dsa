e329c6a3a877d1710d47eca88d0bbf6b542ad177
Arthakker <arthuronyeanusi@gmail.com>
2025-07-27 18:46:29 +0100

    Completed merge sort, moving on to testing...

--- merge_sort.py
+++ merge_sort.py
@@ -10,9 +10,9 @@ def merge(list, left, midpoint, right):
     for j in range(n2):
         R[j] = list[midpoint + 1 + j]
 # Initialize k as starting point of new array
-    k=0
+    k=left
 # Loop to merge based on comparison
-    while i > n1 and j < n2:
+    while i < n1 and j < n2:
         if i < j:
             list[k] = L[i]
             i += 1
@@ -20,12 +20,12 @@ def merge(list, left, midpoint, right):
             list[k] = R[j]
             j += 1
 # If there are remaining elements, in the case that one of the array finishes before the other, resolve that with the following
-    while i > n1:
+    while i < n1:
         list[k] = L[i]
         i += 1
         k += 1
-    while j > n2:
-        list[k] = L[j]
+    while j < n2:
+        list[k] = R[j]
         j += 1
         k += 1
 
@@ -33,11 +33,11 @@ def merge(list, left, midpoint, right):
 
 def merge_sort(list, left, right):
 # Before MergeSort function is applied, we have to make sure that the array is neither empty nor contains a single element
-    if len(list) <= 0:
-        return list
     if left < right:
-        midpoint = left + (right - left) / 2
+        midpoint = left + (right - left) // 2
         merge_sort(list, left, midpoint)
         merge_sort(list, midpoint + 1, right)
-    merge(list, left, midpoint, right)
+        
+        merge(list, left, midpoint, right)
+    
     
