8531d4ea41514356d804f16de27d142ab9c38fe9
Arthakker <arthuronyeanusi@gmail.com>
2025-07-27 18:32:49 +0100

    Understood and implemented mergesort algorithm on an array or as defined by python, a list

--- merge_sort.py
+++ merge_sort.py
@@ -9,17 +9,35 @@ def merge(list, left, midpoint, right):
         L[i] = list[left + i]
     for j in range(n2):
         R[j] = list[midpoint + 1 + j]
-    
+# Initialize k as starting point of new array
+    k=0
+# Loop to merge based on comparison
+    while i > n1 and j < n2:
+        if i < j:
+            list[k] = L[i]
+            i += 1
+        else:
+            list[k] = R[j]
+            j += 1
+# If there are remaining elements, in the case that one of the array finishes before the other, resolve that with the following
+    while i > n1:
+        list[k] = L[i]
+        i += 1
+        k += 1
+    while j > n2:
+        list[k] = L[j]
+        j += 1
+        k += 1
 
 # MergeSort function
 
 def merge_sort(list, left, right):
+# Before MergeSort function is applied, we have to make sure that the array is neither empty nor contains a single element
     if len(list) <= 0:
         return list
     if left < right:
         midpoint = left + (right - left) / 2
         merge_sort(list, left, midpoint)
         merge_sort(list, midpoint + 1, right)
+    merge(list, left, midpoint, right)
     
-    
-# Before MergeSort function is applied, we have to make sure that the array is neither empty nor contains a single element
